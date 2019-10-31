2d2bfd3c158b1ec8d72d2ed7aad6bb59fb1e07b6
Jenny Cueva <jgcuevao@gmail.com>
2019-10-31 16:39:08 -0500

    primer commit

---
+++ src/main/java/com/clinicarest/ClinicaRestApplication.java
@@ -0,0 +1,13 @@
+package com.clinicarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clinicarest/ClinicaRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinicarest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
